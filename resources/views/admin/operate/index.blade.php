7723c7b076184359f4c664c565b6763edf1a96b7
zwkjlyf <zwkjlyf@zwkj.com>
2019-06-12 18:14:17 +0800

    新增resource相关操作与页面

--- resources/views/admin/operate/index.blade.php
+++ resources/views/admin/operate/index.blade.php
@@ -11,14 +11,53 @@
         <div class="layui-tab-content">
             <div class="layui-tab-item layui-show">内容1</div>
             <div class="layui-tab-item">
-                <table id="admin_demo" lay-filter="admin_filter"></table>
+                <table class="layui-table" lay-data="{height:315, url:'/demo/table/user/', page:true, id:'admin_demo'}" lay-filter="admin_filter">
+                    <thead>
+                    <tr>
+                        <th lay-data="{field:'id', width:80, sort: true}">ID</th>
+                        <th lay-data="{field:'username', width:80}">用户名</th>
+                        <th lay-data="{field:'sex', width:80, sort: true}">性别</th>
+                        <th lay-data="{field:'city'}">城市</th>
+                        <th lay-data="{field:'sign'}">签名</th>
+                        <th lay-data="{field:'experience', sort: true}">积分</th>
+                        <th lay-data="{field:'score', sort: true}">评分</th>
+                        <th lay-data="{field:'classify'}">职业</th>
+                        <th lay-data="{field:'wealth', sort: true}">财富</th>
+                    </tr>
+                    </thead>
+                </table>
+{{--                <table id="admin_demo" lay-filter="admin_filter"></table>--}}
             </div>
             <div class="layui-tab-item">内容3</div>
-            <div class="layui-tab-item">内容4</div>
+            <div class="layui-tab-item">
+                <form class="layui-form" action="">
+                    <div class="layui-form-item">
+                        <label class="layui-form-label">管理员名称</label>
+                        <div class="layui-input-block">
+                            <input type="text" name="admin_name" required  lay-verify="required" placeholder="请输入管理员名称" autocomplete="off" class="layui-input">
+                        </div>
+                    </div>
+                    <div class="layui-form-item">
+                        <label class="layui-form-label">密码</label>
+                        <div class="layui-input-inline">
+                            <input type="password" name="password" required lay-verify="required" placeholder="请输入密码" autocomplete="off" class="layui-input">
+                        </div>
+                        <div class="layui-form-mid layui-word-aux">请输入5-10位的英文或数字</div>
+                    </div>
+                    <div class="layui-form-item">
+                        <label class="layui-form-label">性别</label>
+                        <div class="layui-input-block">
+                            <input type="radio" name="sex" value="1" title="男" checked>
+                            <input type="radio" name="sex" value="2" title="女">
+                            <input type="radio" name="sex" value="0" title="保密">
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 
-    <script>
+   {{-- <script>
         layui.use('table', function(){
             var table = layui.table;
 
@@ -42,6 +81,6 @@
             });
 
         });
-    </script>
+    </script>--}}
 
 @endsection
